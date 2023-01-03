f2dc9241caf3ee5188e029d69a54372456737af6
louaitayyar <louaitayyar@outlook.com>
2023-01-03 17:13:49 +0200

    sections and navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,25 @@
 import "./topbar.scss"
+import {Person,Email} from '@mui/icons-material/';
 
 export default function Topbar() {
   return (
-    <div className="topbar">
-        
+    <div className="topbar" id="topbar">
+        <div className="wrapper">
+          <div className="left">
+              <a href="#intro" className="logo">genius.</a>
+              <div className="itemContainer">
+                  <Person className="icon"/>
+                  <span>+3098765432</span>
+              </div>
+              <div className="itemContainer">
+                  <Email className="icon"/>
+                  <span>mail@domain.com</span>
+              </div>
+          </div>
+          <div className="right">
+              This is right
+          </div>
+        </div>
     </div>
   )
 }
