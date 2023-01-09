045074cedb425f7c616753cef7a87dd52034e262
LouaiTayyar <louaitayyar@outlook.com>
2023-01-09 14:05:45 +0200

    added pictures and animation for intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,36 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
 export default function Intro() {
+
+  const textRef = useRef();
+  useEffect(()=>{
+    init(textRef.current,{
+      showCursor: false,
+      strings:['Rupert','Cornelius!','Jack']})
+  },[])
+
+
   return (
     <div className="intro" id="intro"> 
-        
+        <div className="left">
+          <div className="imgContainer">
+              <img src="assets/tylerhd.png" alt=""></img>
+          </div>
+        </div>
+        <div className="right">
+          <div className="nameCard">
+              <h1>HELLO</h1>
+              <h3>my name is</h3>
+              <div className="name">
+                <h2><span ref={textRef}></span></h2>
+              </div>
+          </div>
+          <a href="#portfolio">
+            <img src="assets/down.png" alt=""></img>
+          </a>
+        </div>
     </div>
   )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,7 +6,7 @@ export default function Topbar({menuOpen,setMenuOpen}) {
     <div className={"topbar " + (menuOpen && "active")} id="topbar">
         <div className="wrapper">
           <div className="left">
-              <a href="#intro" className="logo">genius.</a>
+              <a href="#intro" className="logo">Tyler.</a>
               <div className="itemContainer">
                   <Person className="icon"/>
                   <span>+3098765432</span>
