60eafb7ae15df452ca614a6882e58ddb538a6f3f
LouaiTayyar <louaitayyar@outlook.com>
2023-01-09 17:17:54 +0200

    added first item in portfolio. Setup the basic scss

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,19 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-        
+        <h1>Portfolio</h1>
+        <ul>
+          <li className="active">Soap</li>
+          <li>Marla</li>
+          <li>Raymond </li>
+          <li>Lu's Bar</li>
+          <li>Project Mayhem</li>
+        </ul>
+        <div className="container">
+          <div className="item">
+            <img src="assets/soap.png" alt=""/>
+          </div>
+        </div>
     </div>
   )
 }
