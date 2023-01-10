b90799f8effb4a4c18a8f845fd7f786065e34da3
LouaiTayyar <louaitayyar@outlook.com>
2023-01-10 12:24:54 +0200

    added portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,20 +1,89 @@
+import PortfolioList from "../portfolioList/PortfolioList"
 import "./portfolio.scss"
+import { useState, useEffect } from "react";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
 
 export default function Portfolio() {
+
+  const [selected,setSelected] = useState("soap");
+  const [data,setData] = useState([]);
+
+
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ]
+
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "mobile":
+        setData(webPortfolio);
+        break;
+      case "web":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+        break;
+    }
+  },[selected])
+
   return (
     <div className="portfolio" id="portfolio">
         <h1>Portfolio</h1>
         <ul>
-          <li className="active">Soap</li>
-          <li>Marla</li>
-          <li>Raymond </li>
-          <li>Lu's Bar</li>
-          <li>Project Mayhem</li>
+          {list.map((item) => (
+            <PortfolioList 
+            title={item.title} 
+            active={selected === item.id} 
+            setSelected={setSelected}
+            id={item.id}
+            />
+            ))}
         </ul>
         <div className="container">
+          {data.map((d) => (
+
           <div className="item">
-            <img src="assets/soap.png" alt=""/>
+            <img src={d.img} alt=""/>
+            <h3>{d.title}</h3>
           </div>
+
+            ))}
         </div>
     </div>
   )
